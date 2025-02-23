5ebd43b4a4c160e90fa91761f1c42a19b1311dff
Arthur Oliveira <arthur08oliveira01@gmail.com>
2025-02-23 17:25:40 -0300

    Project config

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,8 +1,7 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
